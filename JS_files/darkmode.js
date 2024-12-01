f362a4b7c2a2de39f919c0919f38a668fbb59c23
unknown <ratapakorn@gmail.com>
2024-12-01 12:02:45 +0100

    tt

--- JS_files/darkmode.js
+++ JS_files/darkmode.js
@@ -1,4 +1,18 @@
-function toggleMode(){
+// Toggle dark mode on page load if it's enabled in localStorage
+window.onload = function () {
+    
+    if (localStorage.getItem('darkMode') === 'true') {
+        document.body.classList.toggle('dark-background');
+    }
+};
+
+function toggleMode() {
     document.body.classList.toggle('dark-background');
+    // Save the current state to localStorage
+    if(localStorage.getItem('darkMode') === 'null' || localStorage.getItem('darkMode') === 'false'){
+        localStorage.setItem('darkMode', 'true');
+    }else{
+        localStorage.setItem('darkMode', 'false');
+    }
 }
 
